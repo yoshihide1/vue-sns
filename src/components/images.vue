554da342928e9e565145e38d3a8cb4c0d5ec19a2
yoshihide <aqua.sky.33333@gmail.com>
2020-08-29 17:34:54 +0900

    エラーの修正

--- src/components/images.vue
+++ src/components/images.vue
@@ -4,9 +4,9 @@
     <p>
       <textarea v-model="comment" name id cols="30" rows="10"></textarea>
     </p>
-    <p><input type="file" accept=”image/*” @change="selectImage" /></p>
+    <p><input type="file" accept=”image/*” @change="selectImage"/></p>
     <p>
-      <button id="postButton" @click="upLoad">投稿</button>
+      <button :disabled="postButton" @click="upLoad">投稿</button>
     </p>
   </div>
 </template>
@@ -15,23 +15,26 @@
 import firebase from "../plugins/firebase";
 import { Component, Vue } from "vue-property-decorator";
 
+@Component({})
 export default class Images extends Vue {
   storage = firebase.storage();
   db = firebase.firestore();
   auth = firebase.auth();
-  imageFile: any ;
-  comment: string | null;
+  imageFile: any = "";
+  comment: string | null = "";
+  postButton = false;
 
   selectImage(e: any) {
+    e.preventDefault();
+    console.log(e.target.files[0]);
     this.imageFile = e.target.files[0];
   }
   upLoad() {
-    const button = <HTMLInputElement>document.getElementById("postButton");
     if (!this.imageFile && !this.comment) {
       alert("コメントを入力するか画像を選択してください");
       return;
     }
-    button.disabled = true;
+    this.postButton = true;
     const storageRef = this.storage
       .ref()
       .child(`images/${this.imageFile.name}`);
@@ -43,14 +46,15 @@ export default class Images extends Vue {
         });
         this.imageFile = "";
         this.comment = "";
-        button.disabled = false;
+        this.postButton = false;
         console.log("完了");
       })
       .catch(() => {
-        button.disabled = false;
+        this.postButton = false;
         alert("失敗しました");
       });
   }
+
   urlSave(imageUrl: string) {
     const user = this.auth.currentUser;
     if (user === null) {
