7512d521f5ec9ad5052661ba6f2e00f50f15a2f5
yoshihide <aqua.sky.33333@gmail.com>
2020-08-29 22:25:03 +0900

    storeからのデータ取得追加

--- src/components/images.vue
+++ src/components/images.vue
@@ -8,18 +8,37 @@
     <p>
       <button :disabled="postButton" @click="upLoad">投稿</button>
     </p>
+    <p>
+      <button @click="downLoad">画像取得</button>
+    </p>
+    <!-- <div v-for="image in images" :key="image.timeStamp.second">
+      <img :src="image.imageUrl" alt />
+      <p>{{ image.displayName }}</p>
+      <p>{{ image.uid }}最終的に削除</p>
+    </div>-->
   </div>
 </template>
 
 <script lang="ts">
 import firebase from "../plugins/firebase";
 import { Component, Vue } from "vue-property-decorator";
+interface ImageList {
+  displayName: string;
+  comment?: string;
+  imageUrl?: string;
+  uid: string;
+  timesStamp: {
+    nanoseconds: number;
+    seconds: number;
+  };
+}
 
 @Component({})
 export default class Images extends Vue {
   storage = firebase.storage();
   db = firebase.firestore();
   auth = firebase.auth();
+  images: ImageList[];
   imageFile: any = "";
   comment: string | null = "";
   postButton = false;
@@ -28,6 +47,21 @@ export default class Images extends Vue {
     e.preventDefault();
     console.log(e.target.files[0]);
     this.imageFile = e.target.files[0];
+  }
+  downLoad() {
+    const imageList: any = [];
+    this.db
+      .collection("images")
+      .orderBy("timeStamp", "desc")
+      .limit(5)
+      .get()
+      .then((snapshot) => {
+        snapshot.forEach((doc) => {
+          console.log(doc.id, "=>", doc.data());
+          imageList.push(doc.data());
+        });
+        this.images = imageList;
+      });
   }
   upLoad() {
     if (!this.imageFile && !this.comment) {
